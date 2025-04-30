50b74c7c90a33308f147dc1518be19045d9d5a33
Hassaan Hashmi <hassaan.hashmi@yale.edu>
2025-04-30 15:18:13 -0400

    Update Repo
    Changed arrangement a little

--- aboutme.js
+++ aboutme.js
@@ -2,6 +2,7 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   const app = document.getElementById('typewriter');
+  const img = document.getElementById('profileImage');
 
   const typewriter = new Typewriter(app, {
     loop: false,
@@ -9,13 +10,15 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   typewriter
+    .callFunction(() => {
+      img.classList.add('visible');
+    })
     .typeString(`I am a PhD student at the 
         <a href="https://engineering.yale.edu/academic-study/departments/electrical-and-computer-engineering">Department of ECE</a>, 
         <a href="https://www.yale.edu/">Yale University</a>. I am being advised by <a href="http://dkalogerias.org/">Dionysis Kalogerias</a>. 
         My current research interests are in model-free wireless autonomy and 
         risk-aware reinforcement learning. In a broader sense, I am interested 
         in bilevel stochastic programming emphasizing problem design 
-        and analysis.
-`)
+        and analysis.`)
     .start();
 });
